3fe948de2abc76962fb27eb3ca1bc36687a786ca
Johan Kardell <johan.kardell@collectorbank.se>
2019-11-07 14:53:52 +0100

    Add more packages

--- Linux/prepareNetworkDiagnosticsBox.sh
+++ Linux/prepareNetworkDiagnosticsBox.sh
@@ -3,8 +3,9 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
-
 sudo apt install inetutils-traceroute -y
 sudo apt install tcptraceroute -y
 sudo apt install tcpdump -y
 sudo apt install hping3 -y
+sudo apt install nmap -y
+
